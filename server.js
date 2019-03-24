46ec9a2f6363190c528fb65d466c99d4fca41294
MukundhBhushan <aknsmb@gmail.com>
2019-03-24 15:18:24 +0530

    added blockchain files

--- server.js
+++ server.js
@@ -3,3 +3,39 @@ const base58 = require('bs58')
 const nacl = require('tweetnacl')
 const express = require('express')
 const path = require('path')
+
+//create a variable for creating an instance for server
+let app = express()
+
+app.use(express.static("public"))
+app.set('view engine', 'ejs')
+app.set('views', path.join(_dirname, 'views'))
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.get('/', (req, res) => {
+    res.render('form')
+})
+
+app.post('/posted', (req, res)=> {
+    var mess = req.body.message
+    //create the encryption keys for the user
+    const alice = nacl.sign.keyPair()
+    var publicKey = base58.encode(new Buffer(alice.publicKey))
+    var privateKey = base58.encode(new Buffer(alice.secretKey.slice(0, 32)))
+    const conn = new driver.Connection('https://test.bigchaindb.com/api/v1/')
+    const tx = driver.Transaction.makeCreateTransaction(
+        { message: message },// part to be stored on blockchain
+        {},//this is the meta data},
+        [ driver.Transaction.makeOutput(
+            driver.Transaction.makeEd25519Condition(publicKey))],
+        publicKey)// hashing the data using the keys
+    const txSigned = driver.Transaction.signTransaction(tx, privateKey)
+    console.log(txSigned)
+    conn.postTransactionCommit(txSigned)
+    res.json(txSigned)
+})
+
+app.listen(8000)
